62557c1649cdbd75ce632ab887c893b09646c543
Dan Vega <danvega@gmail.com>
2024-02-23 15:00:46 -0500

    adding starting repository for five

---
+++ src/main/java/dev/danvega/five/TodoFiveRepository.java
@@ -0,0 +1,30 @@
+package dev.danvega.five;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+class TodoFiveRepository {
+
+    private final List<Todo> todos = new ArrayList<>();
+
+    public TodoFiveRepository() {
+        todos.add(new Todo(1,"Hello World!", false));
+    }
+
+    List<Todo> findAll() {
+        return todos;
+    }
+
+    Todo findById(Integer id) {
+        return todos.stream()
+                .filter(todo -> todo.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+
+}
